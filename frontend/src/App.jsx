0aef4450e67113e23566bc4b314a547b74bf6237
bishal ad <animezoneamv@gmail.com>
2025-03-27 13:12:14 +0545

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Route, Routes } from "react-router-dom"
+import Home from './Page/Home/Home'
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Page/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+const Home = () => {
+    return (
+        <div className="text-red-400">
+            <Navbar />
+
+
+
+        </div>
+
+    )
+}
+
+export default Home
